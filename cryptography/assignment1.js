6f942e84bc48a27518784a308b394cea81246b1a
innovativenavneet <navneet.prakash1233@gmail.com>
2025-11-04 23:18:34 +0530

    again starting the journey

---
+++ cryptography/assignment1.js
@@ -0,0 +1,18 @@
+// i have to return a number which when converted to SHA string 
+// generate an output string which starts with '00000'
+const crypto = require('crypto');
+
+function findfunctionstartswithzero(str){
+let input = 0;
+while(true){
+    let inputstr = input.toString();
+    let hashedval = crypto.createHash('sha-256').update(inputstr).digest('hex');
+
+    if(hashedval.startsWith(str)){
+        return {input : input, hash: hashedval}
+    }
+    input++;
+}}
+const result = findfunctionstartswithzero('000');
+console.log("input val",result.input);
+console.log("hashed val",result.hash);
